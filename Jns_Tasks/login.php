abe53644b50b7b79eced7270c2bd3b5742ed9c4c
Jenish Shrestha <mrgeonex@gmail.com>
2024-01-29 16:12:04 +0200

    commiting

---
+++ Jns_Tasks/login.php
@@ -0,0 +1,4 @@
+<?php
+include 'header.php'?>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat in repellat incidunt ex recusandae dolor, quasi nemo vel libero velit eum, iusto temporibus omnis a accusantium enim aut amet tenetur.</p>
+<?php include 'footer.php'?>
